bbb14763da280c0a3922f253c329360b54298301
vashista <vashista.dara03@gmail.com>
2026-06-28 01:18:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
+  connectionTimeout: 60000,
+  greetingTimeout: 30000,
+  socketTimeout: 60000,
 });
 
 transporter.verify((error) => {
