48a8ccc172bb1aebd4c56d07bbb3d4f87b89d6f3
vashista <vashista.dara03@gmail.com>
2026-06-28 01:32:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,16 +20,11 @@ app.use(
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 60000,
-  greetingTimeout: 30000,
-  socketTimeout: 60000,
 });
 
 transporter.verify((error) => {
