20b830a1b513ab0374f5311ad702925cb7863142
vashista <vashista.dara03@gmail.com>
2026-06-28 01:13:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.use(
     credentials: true,
   })
 );
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
