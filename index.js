fc7671312cc23009a2e6fcfec78fd2a7d9708558
vashista <vashista.dara03@gmail.com>
2026-06-28 00:23:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,15 @@ app.use(
   })
 );
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
